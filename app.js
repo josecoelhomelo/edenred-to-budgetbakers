d371573a56c676c82f49bbaad76ab9f085d73018
José Melo <josecoemelo@gmail.com>
2022-11-08 09:30:05 +0000

    improve var terms

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ require('dotenv').config();
 const fs = require('fs');
 const edenred = require('edenred-transactions');
 const budgetbakers = require('budgetbakers-import');
-const edenredLogin = {
+const edenredAuth = {
     host: process.env.EDENRED_HOST,
     username: process.env.EDENRED_USER,
     password: process.env.EDENRED_PASSWORD
 }
-const budgetbakersLogin = {
+const budgetbakersAuth = {
     username: process.env.BUDGETBAKERS_USER,
     password: process.env.BUDGETBAKERS_PASSWORD
 }
-edenred.exportTransactions(edenredLogin.host, edenredLogin.username, edenredLogin.password)
+edenred.exportTransactions(edenredAuth.host, edenredAuth.username, edenredAuth.password)
     .then(res => {
         const data = fs.readFileSync(res, 'utf-8');            
         fs.writeFileSync(res, data.replace('description', 'note'), 'utf-8');
-        return budgetbakers.uploadFile(budgetbakersLogin.username, budgetbakersLogin.password, res, true)
+        return budgetbakers.uploadFile(budgetbakersAuth.username, budgetbakersAuth.password, res, true)
     })
     .then(res => console.log(res));
