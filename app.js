8be752d716370e0505836bcddd71a0e01e19f572
José Melo <josecoemelo@gmail.com>
2022-12-01 02:16:04 +0000

    update retrieval method and mapping

--- app.js
+++ app.js
@@ -11,10 +11,25 @@ const budgetbakersAuth = {
     username: process.env.BUDGETBAKERS_USER,
     password: process.env.BUDGETBAKERS_PASSWORD
 }
-edenred.exportTransactions(edenredAuth.host, edenredAuth.username, edenredAuth.password)
-    .then(res => {
-        const data = fs.readFileSync(res, 'utf-8');            
-        fs.writeFileSync(res, data.replace('description', 'note'), 'utf-8');
-        return budgetbakers.uploadFile(budgetbakersAuth.username, budgetbakersAuth.password, res, true)
+edenred.getTransactions(edenredAuth.host, edenredAuth.username, edenredAuth.password)
+    .then(async res => { 
+        const object = res.map(record => {
+            return {
+                date: record.transactionDate,
+                note: record.transactionName,
+                amount: record.amount >= 0 ? record.amount : 0,
+                expense: record.amount < 0 ? record.amount : 0
+            }
+        });
+          
+        const folder = 'transactions'; 
+        if (!fs.existsSync(folder)) { fs.mkdirSync(folder); }
+        const date = new Date(); 
+        const timestamp = `${date.getFullYear()}-${(`0` + parseInt(date.getMonth()+1)).slice(-2)}-${(`0` + date.getDate()).slice(-2)}T${(`0` + date.getHours()).slice(-2)}-${(`0` + date.getMinutes()).slice(-2)}`;
+        const path = `${folder}/${timestamp}.csv`;        
+        const csv = new ObjectsToCsv(object); 
+        await csv.toDisk(path);
+
+        return budgetbakers.uploadFile(budgetbakersAuth.username, budgetbakersAuth.password, path, true)
     })
     .then(res => console.log(res));
