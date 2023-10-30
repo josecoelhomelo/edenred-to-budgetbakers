a50f0e2f11eb4b2ee1f2cc5764e6b446ed6113d8
José Melo <josecoemelo@gmail.com>
2023-10-30 13:59:31 +0000

    improve var names and update wallet dependency

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This app retrieves transactions from your Edenred's card using module [edenred-transactions](https://github.com/josecoelhomelo/edenred-transactions) and imports them to BudgetBaker's Wallet using module [budgetbakers-import](https://github.com/josecoelhomelo/budgetbakers-import).
+This app retrieves transactions from your Edenred's card using module [edenred-transactions](https://github.com/josecoelhomelo/edenred-transactions) and imports them to BudgetBaker's Wallet using module [wallet-budgetbakers-import](https://github.com/josecoelhomelo/wallet-budgetbakers-import).
 <p><sup>Only tested with the Portuguese host/version (https://www.myedenred.pt).</sup></p>
 
 # Usage
@@ -6,9 +6,11 @@ This app retrieves transactions from your Edenred's card using module [edenred-t
 Create a `.env` file and fill the required information:
 
 ```env
-EDENRED_HOST=
-EDENRED_USER=
-EDENRED_PASSWORD=
-BUDGETBAKERS_USER=
-BUDGETBAKERS_PASSWORD=
+EDENRED_HOST =
+EDENRED_USER =
+EDENRED_PASSWORD =
+WALLET_USER =
+WALLET_PASSWORD =
+WALLET_IMPORT_EMAIL =
+WALLET_ACCOUNT_ID = 
 ```
--- app.js
+++ app.js
@@ -2,15 +2,17 @@ require('dotenv').config();
 const fs = require('node:fs');
 const ObjectsToCsv = require('objects-to-csv');
 const edenred = require('edenred-transactions');
-const budgetbakers = require('budgetbakers-import');
+const wallet = require('wallet-budgetbakers-import');
 const edenredAuth = {
     host: process.env.EDENRED_HOST,
     username: process.env.EDENRED_USER,
     password: process.env.EDENRED_PASSWORD
 }
-const budgetbakersAuth = {
-    username: process.env.BUDGETBAKERS_USER,
-    password: process.env.BUDGETBAKERS_PASSWORD
+const walletInfo = {
+    username: process.env.WALLET_USER,
+    password: process.env.WALLET_PASSWORD,
+    importEmail: process.env.WALLET_IMPORT_EMAIL,
+    accountId: process.env.WALLET_ACCOUNT_ID
 }
 edenred.getTransactions(edenredAuth.host, edenredAuth.username, edenredAuth.password)
     .then(async res => { 
@@ -28,7 +30,7 @@ edenred.getTransactions(edenredAuth.host, edenredAuth.username, edenredAuth.pass
         const timestamp = `${date.getFullYear()}-${(`0` + parseInt(date.getMonth()+1)).slice(-2)}-${(`0` + date.getDate()).slice(-2)}T${(`0` + date.getHours()).slice(-2)}-${(`0` + date.getMinutes()).slice(-2)}`;
         const file = `${folder}/${timestamp}.csv`;        
         const csv = new ObjectsToCsv(transactions); 
-        await csv.toDisk(file);
-        return budgetbakers.uploadFile(budgetbakersAuth.username, budgetbakersAuth.password, file, true);
+        await csv.toDisk(file);     
+        return wallet.uploadFile(walletInfo.username, walletInfo.password, file, walletInfo.importEmail, walletInfo.accountId);
     })
     .then(res => console.log(res));
